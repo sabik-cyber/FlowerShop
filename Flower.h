a7263374953a98459efd70084b85df7371161e8d
sabik-cyber <sabinaturatbek5@gmail.com>
2025-02-03 09:15:55 +0600

    Update Flower.h

--- Flower.h
+++ Flower.h
@@ -1,1 +1,16 @@
+#ifndef FLOWER_H
+#define FLOWER_H
 
+#include <string>
+using namespace std;
+
+class Flower {
+public:
+    string name;
+    string color;
+    float price;
+
+    Flower(string n, string c, float p) : name(n), color(c), price(p) {}
+};
+
+#endif 
