c6a5c9dbb760e6998f19a4056ae5ac87cc483423
sabik-cyber <sabinaturatbek5@gmail.com>
2025-02-03 09:18:02 +0600

    Update warehouse.cpp

--- warehouse.cpp
+++ warehouse.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Flower {
+public:
+    string name;
+    string color;
+    float price;
+
+    Flower(string n, string c, float p) : name(n), color(c), price(p) {}
+};
+
+class FlowerShopWarehouse {
+private:
+    vector<Flower> inventory;
+
+public:
+    void updateInventory(vector<Flower> flowers) {
+        inventory = flowers;
+    }
+
+    void showInventory() {
+        cout << "Current flowers in warehouse:\n";
+        for (auto& flower : inventory) {
+            cout << "Flower: " << flower.name << " Color: " << flower.color << " Price: " << flower.price << endl;
+        }
+    }
+};
 
