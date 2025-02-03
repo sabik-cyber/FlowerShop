14f3ff38ee425e2d637557b964b1032705a294d4
sabik-cyber <sabinaturatbek5@gmail.com>
2025-02-03 09:16:28 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include "admin.cpp"
+#include "client.cpp"
+#include "warehouse.cpp"
+
+using namespace std;
+
+int main() {
+    FlowerShopAdmin admin;
+    FlowerShopClient client;
+    FlowerShopWarehouse warehouse;
+
+    admin.addFlowerToInventory("Роза", "Красный", 100);
+    admin.addFlowerToInventory("Лилия", "Белый", 120);
+    warehouse.updateInventory({Flower("Роза", "Красный", 100), Flower("Лилия", "Белый", 120)});
+    client.setInventory({Flower("Роза", "Красный", 100), Flower("Тюльпан", "Розовый", 90)});
+
+    client.searchFlowerByName("Роза");
+    client.searchFlowerByColor("Розовый");
+
+    warehouse.showInventory();
+
+    return 0;
+}
 
